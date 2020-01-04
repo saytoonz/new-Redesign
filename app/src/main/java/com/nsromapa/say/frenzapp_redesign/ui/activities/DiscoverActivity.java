6ee8a8b98de9e4ad33e474753b45098cb8ab3a15
saytoonz <rassay31@gmail.com>
2020-01-04 12:37:39 +0000

    First Discoveries

---
+++ app/src/main/java/com/nsromapa/say/frenzapp_redesign/ui/activities/DiscoverActivity.java
@@ -0,0 +1,14 @@
+package com.nsromapa.say.frenzapp_redesign.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiscoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover);
+    }
+}
